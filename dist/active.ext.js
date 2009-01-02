daff8398b000ef5803c7169cbb126e8f00be74c5
syntacticx <ryan@syntacticx.com>
2009-01-01 16:43:43 -0800

    updated copyright date to 2009

--- dist/active.ext.js
+++ dist/active.ext.js
@@ -1,6 +1,6 @@
 /* ***** BEGIN LICENSE BLOCK *****
  * 
- * Copyright (c) 2008 Aptana, Inc.
+ * Copyright (c) 2009 Aptana, Inc.
  * 
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
@@ -8009,28 +8009,28 @@ ActiveController.Errors = Errors;
 
 })();
 (function(){
-	var global_context = ActiveSupport.getGlobalContext();
-	var pure = global_context.pure;
-	
-	ActiveController.InstanceMethods.compile = function compile(source_element,name,directives)
-	{
-		return pure.libs.compile(source_element,name,directives,this.toObject());
-	};
-	
-	ActiveController.InstanceMethods.directive = function directive(source_element,directives)
-	{
-		return pure.libs.mapDirective(source_element,directives);
-	};
-	
-	ActiveController.InstanceMethods.render = ActiveSupport.wrap(ActiveController.InstanceMethods.render,function pure_render(proceed,source_element,target,clear){
-		if(content && content.nodeType == 1 && target && target.nodeType == 1 && typeof(clear) === 'object')
-		{
-			var directives = clear;
-			return pure.libs.render(target,this.toObject(),directives,source_element,(typeof(directives) == 'undefined' || !directives));
-		}
-		else
-		{
-			return proceed(source_element,target,clear);
-		}
-	});
+    var global_context = ActiveSupport.getGlobalContext();
+    var pure = global_context.pure;
+    
+    ActiveController.InstanceMethods.compile = function compile(source_element,name,directives)
+    {
+        return pure.libs.compile(source_element,name,directives,this.toObject());
+    };
+    
+    ActiveController.InstanceMethods.directive = function directive(source_element,directives)
+    {
+        return pure.libs.mapDirective(source_element,directives);
+    };
+    
+    ActiveController.InstanceMethods.render = ActiveSupport.wrap(ActiveController.InstanceMethods.render,function pure_render(proceed,source_element,target,clear){
+        if(content && content.nodeType == 1 && target && target.nodeType == 1 && typeof(clear) === 'object')
+        {
+            var directives = clear;
+            return pure.libs.render(target,this.toObject(),directives,source_element,(typeof(directives) == 'undefined' || !directives));
+        }
+        else
+        {
+            return proceed(source_element,target,clear);
+        }
+    });
 })();
