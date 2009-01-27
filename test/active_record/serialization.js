921d78489d21de1d71e260d1a52d8f51292fbe20
syntacticx <ryan@syntacticx.com>
2009-01-26 17:25:47 -0800

    added serialization tests

--- test/active_record/serialization.js
+++ test/active_record/serialization.js
@@ -55,6 +55,28 @@ ActiveTest.Tests.ActiveRecord.serialization = function(proceed)
             });
             var sample = Comment.find(b.id).test_2;
             assert(sample[0] == b.test_2[0] && sample[2][1] == b.test_2[2][1],'Array serialization.');
+            a.destroy();
+            b.destroy();
+            
+            var ted = User.create({name: 'ted'});
+            var one = ted.createComment({title: 'title one',body: 'comment one'});
+            var two = ted.createComment({title: 'title two',body: 'comment two'});
+            //JSON
+            
+            //item
+            assert(ActiveSupport.JSON.parse(ted.toJSON()).name === ted.name && ActiveSupport.JSON.parse(ted.toJSON()).id === ted.id,'JSON parse/serialize item');
+            
+            //array
+            var result = Comment.find({all: true});
+            assert(ActiveSupport.JSON.parse(result.toJSON())[0].body === result[0].body,'JSON parse/serialize array');
+            
+            //nested
+            var json = ted.toJSON({
+              comments: ted.getCommentList().toArray()
+            });
+            var parsed = ActiveSupport.JSON.parse(json);
+            assert(ted.getCommentList()[0].body === parsed.comments[0].body,'JSON parse/serialize object with nested array');
+            
             if(proceed)
                 proceed();
         }
--- test/test.js
+++ test/test.js
@@ -85,7 +85,7 @@ var ActiveTest = {
                         catch(e)
                         {
                             ++ActiveTest.error;
-                            ActiveTest.log('Error' + (ActiveTest.lastNote ? ': ' + ActiveTest.lastNote : ''));
+                            ActiveTest.log('Error after test' + (ActiveTest.lastNote ? ': ' + ActiveTest.lastNote : ''));
                             ActiveTest.log(e);
                             var output = '[' + group_name + ' Pass:' + ActiveTest.pass +',Fail:' + ActiveTest.fail + ',Error:' + ActiveTest.error + ']';
                             ActiveTest.summary.push(output);
@@ -912,6 +912,28 @@ ActiveTest.Tests.ActiveRecord.serialization = function(proceed)
             });
             var sample = Comment.find(b.id).test_2;
             assert(sample[0] == b.test_2[0] && sample[2][1] == b.test_2[2][1],'Array serialization.');
+            a.destroy();
+            b.destroy();
+            
+            var ted = User.create({name: 'ted'});
+            var one = ted.createComment({title: 'title one',body: 'comment one'});
+            var two = ted.createComment({title: 'title two',body: 'comment two'});
+            //JSON
+            
+            //item
+            assert(ActiveSupport.JSON.parse(ted.toJSON()).name === ted.name && ActiveSupport.JSON.parse(ted.toJSON()).id === ted.id,'JSON parse/serialize item');
+            
+            //array
+            var result = Comment.find({all: true});
+            assert(ActiveSupport.JSON.parse(result.toJSON())[0].body === result[0].body,'JSON parse/serialize array');
+            
+            //nested
+            var json = ted.toJSON({
+              comments: ted.getCommentList().toArray()
+            });
+            var parsed = ActiveSupport.JSON.parse(json);
+            assert(ted.getCommentList()[0].body === parsed.comments[0].body,'JSON parse/serialize object with nested array');
+            
             if(proceed)
                 proceed();
         }
