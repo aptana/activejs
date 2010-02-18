d6079e8a30b7c24300cf1a2c67ab2e0e332ee0ba
Ryan Johnson <ryan@Ryan-Johnsons-MacBook-Pro.local>
2010-02-18 15:01:33 -0800

    sprocketized activeview

---
+++ src/active_view.js
@@ -0,0 +1,11 @@
+var ActiveView = null;
+
+(function(){
+
+//= require <active_view/main>
+//= require <active_view/builder>
+//= require <active_view/binding>
+//= require <active_view/layout>
+//= require <active_view/template>
+
+})();
--- src/active_view/header.js
+++
@@ -1,3 +0,0 @@
-var ActiveView = null;
-
-(function(){
