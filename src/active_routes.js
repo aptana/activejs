25b2e8b66b797b0711b81abe4ee445bf54bbc4c9
Ryan Johnson <ryan@Ryan-Johnsons-MacBook-Pro.local>
2010-02-18 14:57:16 -0800

    sprocketized activeroutes

---
+++ src/active_routes.js
@@ -0,0 +1,16 @@
+var ActiveRoutes = null;
+
+if(typeof exports != "undefined"){
+    exports.ActiveRoutes = ActiveRoutes;
+}
+
+(function() {
+
+//=require <active_routes/main>
+//=require <active_routes/errors>
+//=require <active_routes/matching>
+//=require <active_routes/dispatch>
+//=require <active_routes/validations>
+//=require <active_routes/generator>
+
+})();
--- src/active_routes/header.js
+++
@@ -1,7 +0,0 @@
-var ActiveRoutes = null;
-
-if(typeof exports != "undefined"){
-    exports.ActiveRoutes = ActiveRoutes;
-}
-
-(function() {
